6f8c7e84aebc8185942f8c261e79d45f973ab329
jounihuo <jhuopana@gmail.com>
2015-06-03 10:32:05 +0300

    Second commit

--- server.R
+++ server.R
@@ -1,17 +1,23 @@
+#Load shiny and the data
 library(shiny)
 data(LakeHuron)
+
+#Extract the data
 x <- as.numeric(time(LakeHuron))
 y <- as.numeric(LakeHuron)
-
+#Create polygon for fill plot
 xx <- c(x, rev(x))
 yy <- c(rep(0, length(x)), rev(y))
 
+#Initialize the server
 shinyServer(
   function(input, output){
     output$levelPlotFeet <- renderPlot({   
+      #plotting of the water level in feet
       plot(x ,y ,col="darkBlue",type='l',xlab="Year",ylab="Depth in feet",lwd=3,cex.lab=1.5)
       grid(NA, NULL, lwd = 1)
       polygon(xx, yy, col='lightBlue', border=NA)
+      #condtional plotting of the mean and fit lines
       if(length(input$lines)==1 && input$lines=="0"){
         abline(h=mean(x), col="darkBlue")
       }
@@ -25,9 +31,11 @@ shinyServer(
       
     })
     output$levelPlotMeters <- renderPlot({
+      #plotting of the water level in meters
       plot(x ,y*0.3048 ,col="darkBlue",type='l',xlab="Year",ylab="Depth in meters",lwd=3,cex.lab=1.5)
       grid(NA, NULL, lwd = 1)
       polygon(xx, yy*0.3048, col='lightBlue', border=NA)
+      #condtional plotting of the mean and fit lines
       if(length(input$lines)==1 && input$lines=="0"){
         abline(h=mean(y*0.3048), col="darkBlue")
       }
@@ -42,9 +50,7 @@ shinyServer(
 
     })
     output$linyear <- renderText({
-      #if(length(input$button)>0){
-      #  paste(as.character(predict(lm(y ~ x), newdata = data.frame(x = input$year))), " feet.")
-      #}
+      #Calculating the linear estimate based on the year in proper units.
       if(input$button=="1"){
         paste(as.character(round(predict(lm(y*0.3048 ~ x), newdata = data.frame(x = input$year)),1)), " meters.")
       }
--- ui.R
+++ ui.R
@@ -1,11 +1,15 @@
+#Load shiny and dataset
 library(shiny)
 data(LakeHuron)
 
+#Create the UI, include bootstrap.css for theme
 shinyUI(fluidPage(theme = "bootstrap.css",
   headerPanel("Water level visualization"),
   sidebarPanel(
     h3('Analysis options'),
+    #Use documentation
     p('With these controls you can set the plotting parameters on the water level plot. Plot shows annual measurements, in feet, of Lake Huron 1875-1972. To toggle the plot featues, select mean or linear fit. To estimate water level in the past or in the future, set the desired year.'),
+    #Create buttons and check boxes
     radioButtons("button","Water level in",
                        c("feet" = "0",
                          "meters" = "1")),
@@ -14,9 +18,11 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                    "Linear fit" = "1")),
     numericInput('year', 'What year would you like to estimate',1970,min=0,max=3000,step=1),
     strong("Linear estimate of the water level for the given year is:"),
+    #Get the output from the server.R
     textOutput("linyear")
   ),
   mainPanel(
+    #Conditional plotting base on the radio buttons.
     h3('Lake Huron water level'),
     conditionalPanel(
       condition = "input.button == '0'",
